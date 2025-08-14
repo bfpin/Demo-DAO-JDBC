049a4384f8b0d731811acbed23cd505fa2dc4709
Bruno Pinheiro <felipesouza945@gmail.com>
2025-08-14 20:52:57 -0300

    classe vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Departamento dpt = new Departamento (1, "Jamalbee");
+		Vendedor vend = new Vendedor(21,"marcao","marcao@postin",new Date(),2000.00,dpt);
 		
-		System.out.println(dpt);
+		System.out.println(vend);
 	}
 
 }
