a195447e17b3b2d5a4b08971999a37162b622050
Bruno Pinheiro <felipesouza945@gmail.com>
2025-08-14 20:37:11 -0300

    commit departamento

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departamento dpt = new Departamento (1, "Jamalbee");
+		
+		System.out.println(dpt);
+	}
+
+}
